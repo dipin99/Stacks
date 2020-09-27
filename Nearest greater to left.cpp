c739786d082f7bae097a4660a8aeef84dcaa17f4
Heisenberg1999 <dipinahuja99@gmail.com>
2020-09-27 20:51:07 +0530

    ngl

---
+++ Nearest greater to left.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the Size of the Array: ";
+	cin>>n;
+	int a[n];
+	int i,j;
+	cout<<"Enter the Array: ";
+	for(i=0;i<n;i++)
+		cin>>a[i];
+	
+	stack <int> s;
+	vector <int> v;
+	
+	for(i=0;i<n;i++)
+	{
+		if(s.size()==0)
+		{
+			v.push_back(-1);
+		}
+		else if(s.size()>0 && s.top()>a[i])
+		{
+			v.push_back(s.top());
+		}
+		else if(s.size()>0 && s.top()<=a[i] )
+		{
+			while(s.size()>0 && s.top()<=a[i])
+			{
+				s.pop();
+			}
+		
+			if(s.size()==0)
+			{
+				v.push_back(-1);
+			}
+			else
+			{
+				v.push_back(s.top());
+			}
+		}
+		s.push(a[i]);
+	}
+	cout<<"Nearest Greatest Element to the left: ";
+	for(i=0;i<v.size();i++)
+		cout<<v[i]<<" ";
+	
+	
+}
